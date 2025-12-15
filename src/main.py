0650ac75981aea518d5a0397e14512f51b42645e
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-12-15 17:00:54 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,51 @@
 #python 3.10.12
-"""
-Testar se o torch já funciona e depois refatoro o código
-"""
 
-# primeiro commit sempre é para teste
-import testes
+from core.camera import Camera
+from core.hand_tracker import HandTracker
+from core.translator import LibrasTranslator
+from core.signal_buffer import SignalBuffer
+import config
+import time
 
+
+def main():
+    print("Iniciando Sistema de tradução Libras")
+
+
+    camera = Camera()
+    tracker = HandTracker()
+    translator = LibrasTranslator()
+    buffer = SignalBuffer(
+        size=config.BUFFER_SIZE,
+        min_confidence=config.MIN_CONFIDENCE
+    )
+
+    if not config.USE_CAMERA:
+        print("Modo silmulado ativado")
+
+
+        for sign in config.SIMULATED_SIGNS:
+            print(f"[DEBUG] Detectado: {sign}")
+
+            confirmed = buffer.update(sign)
+
+            if confirmed:
+                print(f"Sinal Confirmado: {confirmed}")
+                time.sleep(0.5)
+        print("simulação finalizada")
+        return
+    
+
+    #Modo com webcam (se estiver on)
+    while True:
+        frame = camera.get_frame()
+        landmarks = tracker.dedector(frame)
+        signal = translator.translate(landmarks)
+
+        confirmed = buffer.update(signal)
+
+        if confirmed:
+            print("Sinal Confirmado: ", confirmed)
 
 if __name__ == "__main__":
-    testes()
+    main()
