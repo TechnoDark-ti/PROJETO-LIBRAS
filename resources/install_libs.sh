bee53d70539d1104bf5f4fd32e1527817b7eacc1
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-12-15 20:59:20 -0300

    Create install_libs.sh

---
+++ resources/install_libs.sh
@@ -0,0 +1,3 @@
+pip3 install opencv-python opencv-contrib-python cvzone mediapipe flet
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install numpy pillow matplotlib
