04417eb8bfc9b102f858d73f89ed49b7f15cbf68
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-12-15 20:54:20 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,50 +2,36 @@
 
 from core.camera import Camera
 from core.hand_tracker import HandTracker
-from core.translator import LibrasTranslator
+from core.translator import Translator
 from core.signal_buffer import SignalBuffer
 import config
-import time
 
 
 def main():
-    print("Iniciando Sistema de tradução Libras")
+    """
+    Ponto de entrada do sistema.
+    Apenas inicializa e conecta os módulos.
+    """
 
-
+    # Inicialização dos módulos
     camera = Camera()
-    tracker = HandTracker()
-    translator = LibrasTranslator()
-    buffer = SignalBuffer(
+    hand_tracker = HandTracker()
+    translator = Translator(model_path="models/libras_model.pt")
+    signal_buffer = SignalBuffer(
         size=config.BUFFER_SIZE,
         min_confidence=config.MIN_CONFIDENCE
     )
 
-    if not config.USE_CAMERA:
-        print("Modo silmulado ativado")
-
-
-        for sign in config.SIMULATED_SIGNS:
-            print(f"[DEBUG] Detectado: {sign}")
-
-            confirmed = buffer.update(sign)
-
-            if confirmed:
-                print(f"Sinal Confirmado: {confirmed}")
-                time.sleep(0.5)
-        print("simulação finalizada")
-        return
-    
-
-    #Modo com webcam (se estiver on)
-    while True:
+    # Loop principal (inativo sem webcam)
+    while False:
         frame = camera.get_frame()
-        landmarks = tracker.dedector(frame)
+        landmarks = hand_tracker.process(frame)
         signal = translator.translate(landmarks)
+        confirmed_signal = signal_buffer.update(signal)
 
-        confirmed = buffer.update(signal)
+        if confirmed_signal:
+            print("Sinal confirmado:", confirmed_signal)
 
-        if confirmed:
-            print("Sinal Confirmado: ", confirmed)
 
 if __name__ == "__main__":
     main()
