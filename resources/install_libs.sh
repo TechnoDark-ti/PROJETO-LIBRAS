edd927b9d4f7f51796ef70508cb502c6434efd24
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-12-16 11:55:59 -0300

    Update install_libs.sh

--- resources/install_libs.sh
+++ resources/install_libs.sh
@@ -1,3 +1,7 @@
-pip3 install opencv-python opencv-contrib-python cvzone mediapipe flet
+pip3 install opencv-python opencv-contrib-python cvzone mediapipe flet numpy pillow matplotlib
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip3 install numpy pillow matplotlib
+
+
+#sudo apt install python3-pip python3-virtualenv -y
+sudo apt install -y libgtk-3-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
+                    libmpv-dev mpv
