f99ffd78114af1e602285b354b6bef8fe8b9ac04
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-12-27 13:24:32 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,9 @@
 # config.py
+"""
+Docstring para config
 
+Arquivo com paremetros para o funcionamento de cemaras e alguns configs de desenvolvimento do meu ambiente
+"""
 # Aqui é a minha gambiarra para ativar a camera
 DEBUG_MODE = False
 USE_CAMERA = True
@@ -8,6 +12,7 @@ BUFFER_SIZE = 7
 MIN_CONFIDENCE = 0.7
 
 # CORREÇÃO: Renomeado para SIMULATED_SIGNS (plural), pois contém múltiplos sinais
+#Trecho não utilizado, era para teste sem camera
 SIMULATED_SIGNS = [
     "A", "A", "A", "A",
     "B", "B", "B",
